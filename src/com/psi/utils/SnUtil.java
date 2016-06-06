aa5e6581e9286f42970d546cde42633c5c854335
maxiaosha <2087687197@qq.com>
2016-06-06 22:59:45 +0800

    Update SnUtil.java

--- src/com/psi/utils/SnUtil.java
+++ src/com/psi/utils/SnUtil.java
@@ -15,20 +15,14 @@ public class SnUtil {
 		java.util.Date date=new java.util.Date();
 		String str=sdf.format(date);
 		s+=str;
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < 6; i++) {
-            int a = Math.abs((new Random()).nextInt(57));// 产生0~57的随机数
-            if (a <= 9) {// 将0~9转为char的0~9
-                sb.append((char) (a + 48));
-            } else if (a < 33) {// 将10~33转为char的A~Z
-                if((a + 55) == 79 || (a + 55) == 73){
-                    sb.append((char) (a + 63));
-                }else{
-                    sb.append((char) (a + 55));
-                }
-            }
+       StringBuffer buffer = new StringBuffer("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");   
+        StringBuffer sb = new StringBuffer();   
+        Random random = new Random();   
+        int range = buffer.length();   
+        for (int i = 0; i < 6; i ++) {   
+            sb.append(buffer.charAt(random.nextInt(range)));   
         }
-		s+=sb;
+        s+=sb;
 		return s;
 	}
 
